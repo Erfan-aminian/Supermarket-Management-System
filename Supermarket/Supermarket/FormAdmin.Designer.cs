2d9acbf2b9596fda79f7109ef79e77d8e1fd199b
Erfan-Aminian <aminian158@gmail.com>
2025-09-29 19:58:54 +0330

    Implement Category CRUD buttons with user input handling

--- Supermarket/Supermarket/FormAdmin.Designer.cs
+++ Supermarket/Supermarket/FormAdmin.Designer.cs
@@ -31,6 +31,12 @@
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
             Category = new TabPage();
+            textBoxCategory = new TextBox();
+            labelCategory = new Label();
+            buttonRefresh1 = new Button();
+            buttonDelete1 = new Button();
+            buttonUpdate1 = new Button();
+            buttonAdd1 = new Button();
             dataGridViewCategory = new DataGridView();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
@@ -49,7 +55,7 @@
             tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(946, 523);
+            tabControl1.Size = new Size(941, 523);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -58,33 +64,99 @@
             tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(938, 490);
+            tabPage1.Size = new Size(933, 490);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Products";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // Category
             // 
+            Category.Controls.Add(textBoxCategory);
+            Category.Controls.Add(labelCategory);
+            Category.Controls.Add(buttonRefresh1);
+            Category.Controls.Add(buttonDelete1);
+            Category.Controls.Add(buttonUpdate1);
+            Category.Controls.Add(buttonAdd1);
             Category.Controls.Add(dataGridViewCategory);
             Category.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Category.Location = new Point(4, 29);
             Category.Name = "Category";
-            Category.Size = new Size(938, 490);
+            Category.Size = new Size(933, 490);
             Category.TabIndex = 3;
             Category.Text = "Cateory";
             Category.UseVisualStyleBackColor = true;
             // 
+            // textBoxCategory
+            // 
+            textBoxCategory.Location = new Point(642, 233);
+            textBoxCategory.Name = "textBoxCategory";
+            textBoxCategory.Size = new Size(243, 34);
+            textBoxCategory.TabIndex = 6;
+            // 
+            // labelCategory
+            // 
+            labelCategory.AutoSize = true;
+            labelCategory.Location = new Point(642, 17);
+            labelCategory.Name = "labelCategory";
+            labelCategory.Size = new Size(65, 28);
+            labelCategory.TabIndex = 5;
+            labelCategory.Text = "label1";
+            // 
+            // buttonRefresh1
+            // 
+            buttonRefresh1.Location = new Point(791, 406);
+            buttonRefresh1.Name = "buttonRefresh1";
+            buttonRefresh1.Size = new Size(94, 44);
+            buttonRefresh1.TabIndex = 4;
+            buttonRefresh1.Text = "Refresh";
+            buttonRefresh1.UseVisualStyleBackColor = true;
+            buttonRefresh1.Click += buttonRefresh1_Click;
+            // 
+            // buttonDelete1
+            // 
+            buttonDelete1.Location = new Point(655, 406);
+            buttonDelete1.Name = "buttonDelete1";
+            buttonDelete1.Size = new Size(94, 44);
+            buttonDelete1.TabIndex = 3;
+            buttonDelete1.Text = "Delete";
+            buttonDelete1.UseVisualStyleBackColor = true;
+            buttonDelete1.Click += buttonDelete1_Click;
+            // 
+            // buttonUpdate1
+            // 
+            buttonUpdate1.Location = new Point(791, 318);
+            buttonUpdate1.Name = "buttonUpdate1";
+            buttonUpdate1.Size = new Size(94, 44);
+            buttonUpdate1.TabIndex = 2;
+            buttonUpdate1.Text = "Update";
+            buttonUpdate1.UseVisualStyleBackColor = true;
+            buttonUpdate1.Click += buttonUpdate1_Click;
+            // 
+            // buttonAdd1
+            // 
+            buttonAdd1.Location = new Point(655, 318);
+            buttonAdd1.Name = "buttonAdd1";
+            buttonAdd1.Size = new Size(94, 44);
+            buttonAdd1.TabIndex = 1;
+            buttonAdd1.Text = "Add";
+            buttonAdd1.UseVisualStyleBackColor = true;
+            buttonAdd1.Click += buttonAdd1_Click;
+            // 
             // dataGridViewCategory
             // 
             dataGridViewCategory.AllowUserToDeleteRows = false;
-            dataGridViewCategory.BackgroundColor = SystemColors.ControlLight;
+            dataGridViewCategory.AllowUserToResizeRows = false;
+            dataGridViewCategory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCategory.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCategory.BackgroundColor = Color.Gainsboro;
             dataGridViewCategory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCategory.GridColor = SystemColors.InactiveBorder;
-            dataGridViewCategory.Location = new Point(16, 14);
+            dataGridViewCategory.Dock = DockStyle.Left;
+            dataGridViewCategory.GridColor = Color.White;
+            dataGridViewCategory.Location = new Point(0, 0);
             dataGridViewCategory.Name = "dataGridViewCategory";
             dataGridViewCategory.ReadOnly = true;
             dataGridViewCategory.RowHeadersWidth = 51;
-            dataGridViewCategory.Size = new Size(884, 453);
+            dataGridViewCategory.Size = new Size(600, 490);
             dataGridViewCategory.TabIndex = 0;
             // 
             // tabPage2
@@ -93,7 +165,7 @@
             tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(938, 490);
+            tabPage2.Size = new Size(933, 490);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Cart";
             tabPage2.UseVisualStyleBackColor = true;
@@ -103,7 +175,7 @@
             tabPage3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             tabPage3.Location = new Point(4, 29);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(938, 490);
+            tabPage3.Size = new Size(933, 490);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Cart Item";
             tabPage3.UseVisualStyleBackColor = true;
@@ -118,6 +190,7 @@
             Text = "FormAdmin";
             tabControl1.ResumeLayout(false);
             Category.ResumeLayout(false);
+            Category.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewCategory).EndInit();
             ResumeLayout(false);
         }
@@ -130,5 +203,11 @@
         private TabPage tabPage3;
         private TabPage Category;
         private DataGridView dataGridViewCategory;
+        private Button buttonRefresh1;
+        private Button buttonDelete1;
+        private Button buttonUpdate1;
+        private Button buttonAdd1;
+        private Label labelCategory;
+        private TextBox textBoxCategory;
     }
 }
--- Supermarket/Supermarket/FormAdmin.cs
+++ Supermarket/Supermarket/FormAdmin.cs
@@ -62,6 +62,110 @@ namespace Supermarket
             }
         }
 
+        private void buttonRefresh1_Click(object sender, EventArgs e)
+        {
+            LoadCategoryData();
+            MessageBox.Show("Refresh");
+        }
+
+        private void buttonAdd1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=.;Database=SupermarketDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            string name = textBoxCategory.Text;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Category (name) VALUES (@name)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@name", name);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("yor value added.");
+                LoadCategoryData();
+
+
+            }
+
+        }
+        private void dataGridViewCategory_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewCategory.CurrentRow != null)
+            {
+                // مقدار فعلی ستون Name رو میاریم و تو TextBox میذاریم
+                textBoxCategory.Text = dataGridViewCategory.CurrentRow.Cells["Name"].Value.ToString();
+            }
+        }
+
+        private void buttonUpdate1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=.;Database=SupermarketDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            if (dataGridViewCategory.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dataGridViewCategory.CurrentRow.Cells["categoryId"].Value);
+                string name = textBoxCategory.Text;
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Category SET Name = @Name where CategoryId = @Id";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Your field update");
+                    LoadCategoryData();
+
+                }
+
+
+
+            }
+
+        }
+
+        private void buttonDelete1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=.;Database=SupermarketDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            string input = textBoxCategory.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Please enter ID or Name to delete.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                if (int.TryParse(input, out int id))
+                {
+                    cmd.CommandText = "DELETE FROM Category WHERE CategoryId = @Value";
+                    cmd.Parameters.AddWithValue("@Value", id);
+                }
+                else
+                {
+                    cmd.CommandText = "DELETE FROM Category WHERE Name = @Value";
+                    cmd.Parameters.AddWithValue("@Value", input);
+                }
+                conn.Open();
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                    MessageBox.Show("Deleted successfully!");
+                else
+                    MessageBox.Show("No matching record found.");
+
+                LoadCategoryData();
+                textBoxCategory.Clear();
+
+
+
+
+
+            }
+
+        }
     }
 }
 
