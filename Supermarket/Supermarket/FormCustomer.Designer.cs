6afabc264201b1e1155af401ef0ad9f53f989510
Erfan-Aminian <aminian158@gmail.com>
2025-10-01 22:20:24 +0330

    Add products in form customer

--- Supermarket/Supermarket/FormCustomer.Designer.cs
+++ Supermarket/Supermarket/FormCustomer.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormCustomer";
+            tabControlCustomer = new TabControl();
+            Products0 = new TabPage();
+            buttonProductAdd = new Button();
+            label1 = new Label();
+            dataGridViewProducts = new DataGridView();
+            tabPageCart = new TabPage();
+            tabControlCustomer.SuspendLayout();
+            Products0.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControlCustomer
+            // 
+            tabControlCustomer.Controls.Add(Products0);
+            tabControlCustomer.Controls.Add(tabPageCart);
+            tabControlCustomer.Location = new Point(12, 12);
+            tabControlCustomer.Name = "tabControlCustomer";
+            tabControlCustomer.SelectedIndex = 0;
+            tabControlCustomer.Size = new Size(879, 470);
+            tabControlCustomer.TabIndex = 0;
+            // 
+            // Products0
+            // 
+            Products0.Controls.Add(buttonProductAdd);
+            Products0.Controls.Add(label1);
+            Products0.Controls.Add(dataGridViewProducts);
+            Products0.Location = new Point(4, 29);
+            Products0.Name = "Products0";
+            Products0.Padding = new Padding(3);
+            Products0.Size = new Size(871, 437);
+            Products0.TabIndex = 0;
+            Products0.Text = "Products";
+            Products0.UseVisualStyleBackColor = true;
+            // 
+            // buttonProductAdd
+            // 
+            buttonProductAdd.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonProductAdd.Location = new Point(638, 324);
+            buttonProductAdd.Name = "buttonProductAdd";
+            buttonProductAdd.Size = new Size(198, 74);
+            buttonProductAdd.TabIndex = 2;
+            buttonProductAdd.Text = "Add to cart";
+            buttonProductAdd.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(622, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // dataGridViewProducts
+            // 
+            dataGridViewProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewProducts.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProducts.Dock = DockStyle.Left;
+            dataGridViewProducts.Location = new Point(3, 3);
+            dataGridViewProducts.Name = "dataGridViewProducts";
+            dataGridViewProducts.RowHeadersWidth = 51;
+            dataGridViewProducts.Size = new Size(589, 431);
+            dataGridViewProducts.TabIndex = 0;
+            // 
+            // tabPageCart
+            // 
+            tabPageCart.Location = new Point(4, 29);
+            tabPageCart.Name = "tabPageCart";
+            tabPageCart.Padding = new Padding(3);
+            tabPageCart.Size = new Size(871, 437);
+            tabPageCart.TabIndex = 1;
+            tabPageCart.Text = "Cart";
+            tabPageCart.UseVisualStyleBackColor = true;
+            // 
+            // FormCustomer
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(903, 494);
+            Controls.Add(tabControlCustomer);
+            Name = "FormCustomer";
+            Text = "FormCustomer";
+            tabControlCustomer.ResumeLayout(false);
+            Products0.ResumeLayout(false);
+            Products0.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControlCustomer;
+        private TabPage Products0;
+        private DataGridView dataGridViewProducts;
+        private TabPage tabPageCart;
+        private Button buttonProductAdd;
+        private Label label1;
     }
 }
--- Supermarket/Supermarket/FormCustomer.cs
+++ Supermarket/Supermarket/FormCustomer.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
 
 namespace Supermarket
 {
@@ -15,6 +17,41 @@ namespace Supermarket
         public FormCustomer()
         {
             InitializeComponent();
+            this.Load += FormCustomer_Load;
+        }
+        private void FormCustomer_Load(object sender, EventArgs e)
+        {
+            LoadProductsData();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControlCustomer.SelectedTab == Products0) // اسم تبت رو درست بذار
+            {
+                LoadProductsData();
+            }
+        }
+
+        private void LoadProductsData()
+        {
+            string connectionString = @"Server=.;Database=SupermarketDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM Products";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridViewProducts.DataSource = dt;
+                    //MessageBox.Show("connect !");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
         }
     }
 }
